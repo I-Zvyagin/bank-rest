2f711cf0cc00a00613aa920e94957fe9ece2d959
i_zvyagin <vanyazvyaginfr@gmail.com>
2025-10-06 20:40:03 +0300

    Added AuthenticationService

---
+++ src/main/java/com/example/bankcards/service/AuthenticationService.java
@@ -0,0 +1,54 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.JwtAuthenticationResponse;
+import com.example.bankcards.dto.SignInRequest;
+import com.example.bankcards.dto.SignUpRequest;
+import com.example.bankcards.entity.RoleName;
+import com.example.bankcards.entity.UserEntity;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(access =  AccessLevel.PACKAGE)
+public class AuthenticationService {
+    private final UserService userService;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+
+    //Регистрация пользователя
+    public JwtAuthenticationResponse signUp(SignUpRequest request) {
+
+        var user = UserEntity.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(RoleName.USER)
+                .build();
+
+        userService.createUser(user);
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    //Аутентификация пользователя
+    public JwtAuthenticationResponse signIn(SignInRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getUsername(),
+                request.getPassword()
+        ));
+
+        var user = userService
+                .userDetailsService()
+                .loadUserByUsername(request.getUsername());
+
+        var jwt = jwtService.generateToken(user);
+        return new JwtAuthenticationResponse(jwt);
+    }
+}
--- src/main/java/com/example/bankcards/service/UserService.java
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.bankcards.service;
 
 import com.example.bankcards.entity.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService {
     UserEntity saveUser(UserEntity user);
 
     UserEntity createUser(UserEntity user);
+
+    UserDetailsService userDetailsService();
 }
