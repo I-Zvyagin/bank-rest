f4a8ec71c064fe2b639c68b8dacf4a989eb8e94e
i_zvyagin <vanyazvyaginfr@gmail.com>
2025-10-07 10:09:32 +0300

    Added card repository

---
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -0,0 +1,12 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.CardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CardRepository extends JpaRepository <CardEntity, Long> {
+
+    Optional<CardEntity> findByCardNumber(String cardNumber);
+
+}
