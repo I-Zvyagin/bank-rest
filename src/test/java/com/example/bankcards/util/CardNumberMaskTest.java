d6fe7535fffeab437449ba046d74ec642ec8ca05
i_zvyagin <vanyazvyaginfr@gmail.com>
2026-06-12 01:33:18 +0300

    test: add a card mask display test

---
+++ src/test/java/com/example/bankcards/util/CardNumberMaskTest.java
@@ -0,0 +1,19 @@
+package com.example.bankcards.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CardNumberMaskTest {
+    private CardNumberMask cardNumberMask;
+
+    @Test
+    void shouldReturnCardMask() {
+        String cardNumber = "1234567887654321";
+
+        assertEquals("**** **** **** 4321", CardNumberMask.getMaskedCardNumber(cardNumber));
+    }
+}
